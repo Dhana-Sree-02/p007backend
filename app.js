ca1991c991c25a54f0e733058b71b8c86c89d89e
Dhana-Sree-02 <dhanasree0238@gmail.com>
2026-02-27 10:32:52 +0530

    .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import usersRouter from '../backend/controllers/usersController.js';
+import usersRouter from './controllers/usersController.js';
+
 
 
 const app = express();
