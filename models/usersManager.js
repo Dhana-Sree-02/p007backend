ef285746a0115f35a199288fd50fa90abde84c44
Dhana-Sree-02 <dhanasree0238@gmail.com>
2026-03-27 18:39:09 +0530

    .

--- models/usersManager.js
+++ models/usersManager.js
@@ -1,43 +1,52 @@
 import { ObjectId } from 'mongodb';
 import { connectDB } from '../config/db.js';
 
-export async function getAllUsers(){
+export async function getAllUsers() {
     const db = await connectDB();
     const users = await db.collection("users").find().toArray();
     return users;
 }
 
-export async function deleteUser(_id){
+export async function deleteUser(_id) {
     const db = await connectDB();
     const users = await db.collection("users");
-    await users.deleteOne({_id: new ObjectId(_id)});
-    return {msg: "Deleted successfully"};
+    await users.deleteOne({ _id: new ObjectId(_id) });
+    return { msg: "Deleted successfully" };
 }
 
 export async function updateUser(params) {
     const db = await connectDB();
     const users = await db.collection("users");
-    
-    const updateDoc = {$set:{
-                            firstname: params.firstname, 
-                            lastname: params.lastname, 
-                            mobile: params.mobile, 
-                            email: params.email
-                        }};
-    await users.findOneAndUpdate({_id: new ObjectId(params._id)}, updateDoc);
 
-    return {msg: "Updated successfully"};
+    const updateDoc = {
+        $set: {
+        id: params.id,
+        firstname: params.firstname,
+        lastname: params.lastname,
+        mobile: params.mobile,
+        email: params.email,
+        dept: params.dept,
+        address: params.address,
+        password: params.password
+        }
+    };
+    await users.findOneAndUpdate({ _id: new ObjectId(params._id) }, updateDoc);
+
+    return { msg: "Updated successfully" };
 }
 
-export async function saveUser(params){
+export async function saveUser(params) {
     const db = await connectDB();
     const users = await db.collection("users");
     await users.insertOne({
-        firstname: params.firstname, 
-        lastname: params.lastname, 
-        mobile: params.mobile, 
+        id: params.id,
+        firstname: params.firstname,
+        lastname: params.lastname,
+        mobile: params.mobile,
         email: params.email,
+        dept: params.dept,
+        address: params.address,
         password: params.password
     });
-    return {msg: "Saved successfully"};
+    return { msg: "Saved successfully" };
 }
