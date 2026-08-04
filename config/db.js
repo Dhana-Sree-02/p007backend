56961ffa0891b5bf6881ac1de4a758814ff88081
Dhana-Sree-02 <dhanasree0238@gmail.com>
2026-08-04 17:07:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const url = "mongodb+srv://dhanasree02:dhanasree02@cluster0.ipq8tqd.mongodb.net/?appName=Cluster0";
+const url = "mongodb+srv://dhanasree02:dhanasree02@cluster0.2asglpo.mongodb.net/?appName=Cluster0";
 const dbname = "fedf";
 
 const client = new MongoClient(url);
